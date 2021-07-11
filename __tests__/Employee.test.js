ecd875181df0a542c25af71cfd96d6cccd0379ce
Iryna <iryna.shulim@gmail.com>
2021-07-11 15:01:49 -0500

    add a hint for future tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,7 +5,7 @@ const Employee = require("../lib/Employee")
 test('creates an Employee object', () => {
     const employee = new Employee("Bob",19,"email@email.com");
     
-    expect(employee.name).toBe("Bob");
+    expect(employee.name).toBe("Bob");// if you want to test for any string test gonna look like: expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.getRole()).toBe("Employee");
