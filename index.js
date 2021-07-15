3ab3375487c9dcd6f7eb14e7d705b9281258bde8
Iryna <iryna.shulim@gmail.com>
2021-07-15 10:45:31 -0500

    fixing bugs, working node prompts

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const teamMembers = [];
+const idArray = [];
 
 function appMenu() {
 
